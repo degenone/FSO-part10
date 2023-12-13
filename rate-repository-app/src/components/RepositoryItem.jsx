871ba3a3d8c61a2187fffe40a0421e45132e8c2a
Tero Kilpeläinen <degenone1@gmail.com>
2023-12-13 17:09:07 +0200

    exercise 10.5 done

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,17 +1,61 @@
-import { View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import { Subheading, Text } from './Typography';
+import theme from '../theme';
+import StatDisplay from './StatDisplay';
+
+const styles = StyleSheet.create({
+    container: {
+        margin: 10,
+    },
+    flexRow: { flexDirection: 'row' },
+    spaceEvenly: { justifyContent: 'space-evenly' },
+    mb5: { marginBottom: 5 },
+    avatar: {
+        width: 50,
+        height: 50,
+        marginEnd: 10,
+        borderRadius: 5,
+    },
+    description: {
+        flexShrink: 1,
+        justifyContent: 'space-between',
+        alignItems: 'baseline',
+    },
+    language: {
+        backgroundColor: theme.colors.primary,
+        color: '#f3f3f3',
+        borderRadius: 5,
+        padding: 5,
+    },
+});
 
 const RepositoryItem = (props) => {
     const { repo } = props;
     return (
-        <View>
-            <Subheading color='primary'>Full name: {repo.fullName}</Subheading>
-            <Text color='textSecondary'>Description: {repo.description}</Text>
-            <Text>Language: {repo.language}</Text>
-            <Text>Forks: {repo.forksCount}</Text>
-            <Text>Stars: {repo.stargazersCount}</Text>
-            <Text>Rating: {repo.ratingAverage}</Text>
-            <Text>Reviews: {repo.reviewCount}</Text>
+        <View style={styles.container}>
+            <View style={styles.flexRow}>
+                <Image
+                    source={{ uri: repo.ownerAvatarUrl }}
+                    style={styles.avatar}
+                />
+                <View style={styles.description}>
+                    <Subheading color='primary' style={styles.mb5}>
+                        {repo.fullName}
+                    </Subheading>
+                    <Text color='textSecondary' style={styles.mb5}>
+                        {repo.description}
+                    </Text>
+                    <Text style={[styles.language, styles.mb5]}>
+                        {repo.language}
+                    </Text>
+                </View>
+            </View>
+            <View style={[styles.flexRow, styles.spaceEvenly]}>
+                <StatDisplay number={repo.forksCount} text='Forks' />
+                <StatDisplay number={repo.stargazersCount} text='Stars' />
+                <StatDisplay number={repo.ratingAverage} text='Rating' />
+                <StatDisplay number={repo.reviewCount} text='Reviews' />
+            </View>
         </View>
     );
 };
---
+++ rate-repository-app/src/components/StatDisplay.jsx
@@ -0,0 +1,25 @@
+import { View } from 'react-native';
+import { Text } from './Typography';
+
+const StatDisplay = (props) => {
+    const { number, text } = props;
+    const prettifyNumber = (number) => {
+        if (number < 1000) {
+            return number.toString();
+        }
+        const thousands = (number / 1000).toFixed(0);
+        const remainder = ((number % 1000) / 100).toFixed(0);
+        if (remainder > 0) {
+            return `${thousands},${remainder} k`;
+        }
+        return `${thousands}k`;
+    };
+    return (
+        <View style={{ alignItems: 'center' }}>
+            <Text fontWeight='bold'>{prettifyNumber(number)}</Text>
+            <Text>{text}</Text>
+        </View>
+    );
+};
+
+export default StatDisplay;
